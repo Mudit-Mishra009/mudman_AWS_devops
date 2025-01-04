60ca500bb8e0a9e5a83de662cf399cfa0a5534b5
Mudit-Mishra009 <67537212+Mudit-Mishra009@users.noreply.github.com>
2025-01-04 22:55:59 +0530

    Delete we-shopify-platform directory
    Deleting for practice

--- we-shopify-platform/src/main/java/com/weshopify/platform/features/customers/commons/CustomerConstants.java
+++
@@ -1,7 +0,0 @@
-package com.weshopify.platform.features.customers.commons;
-
-public interface CustomerConstants {
-
-	String SEARCH_KEY_SEPERATOR=",";
-	String DEFAULT_SORT_ORDER_BY_EMAIL = "email";
-}
